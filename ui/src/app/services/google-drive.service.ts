a71a1b8c1c22891b4083d4f5d0b99aa68b13af41
Hen <bandel79@gmail.com>
2026-05-11 10:28:58 +0300

    feat: implement GoogleDriveService for OAuth2 authentication and file retrieval

--- ui/src/app/services/google-drive.service.ts
+++ ui/src/app/services/google-drive.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { BehaviorSubject, Observable, from } from 'rxjs';
-import { environment } from '../../../environments/environment';
+import { environment } from '../../environments/environment';
 
 declare var google: any;
 
